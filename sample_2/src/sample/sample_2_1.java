fb6f8b6021b966b2bf24f7d70114875d415afa46
Mrinalika <mrinalika.bhumca2014@gmail.com>
2015-03-27 14:31:44 -0700

    sample

---
+++ sample_2/src/sample/sample_2_1.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package sample;
+
+/**
+ * @author Dell
+ *
+ */
+public class sample_2_1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int a=100,b=50,c;
+		c=a/b;
+		System.out.println(c);
+		// TODO Auto-generated method stub
+
+	}
+
+}
